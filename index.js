227c5477d4631a986fb2cf310120dd79ad3fbadc
DOROTHY2322 <kimelidorothy@gmail.com>
2022-11-17 21:22:18 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+var a,b
+
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(a){
+    return a+1;
+}
+function decrement(a){
+    return a-1;
+}
+    function makeInt(n){
+        return 
+    }
+    function makeInt(a){
+        return a;
+    }
+    function makeInt(string){
+        return parseInt(string,10)
+      }
+
+      function preserveDecimal(string){
+        return parseFloat(string)
+}
+    
+    
